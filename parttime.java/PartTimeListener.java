67c36095a272bb5d21e54ad6a082a7304a51d0bd
2370192 <s2370192@s.do-johodai.ac.jp>
2024-06-10 16:19:55 +0900

    Delete parttime.java directory

--- parttime.java/PartTimeListener.java
+++
@@ -1,9 +0,0 @@
-
-public class PartTimeListener {
-
-    public void onEarningsUpdated(double totalEarnings, double totalWorkTime) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'onEarningsUpdated'");
-    }
-
-}
